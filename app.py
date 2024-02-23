440f906126f4daaf5cda36f5380629ebdf69d048
Stephano <stegoodds@gmail.com>
2024-02-22 22:43:50 -0300

    Update def get_tasks

--- app.py
+++ app.py
@@ -17,6 +17,16 @@ def create_task():
     print(tasks)
     return jsonify({"message": "Nova tarefa criada com sucesso"})
 
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    task_list = [task.to_dict() for task in tasks]
+
+    output = {
+                "tasks": task_list,
+                "total_tasks": 0
+                }
+    return jsonify(output)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
