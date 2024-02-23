49ba36abd06da5add83440fa24a1b216f7995eeb
Stephano <stegoodds@gmail.com>
2024-02-22 22:22:41 -0300

    Add task insert

--- app.py
+++ app.py
@@ -1,16 +1,21 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from models.task import Task
 
 #__name_- = "__main__"
 app = Flask(__name__)
 
 tasks = []
+task_id_control = 1
 
 @app.route('/tasks', methods=['POST'])
 def create_task():
+    global task_id_control
     data = request.get_json()
-    print(data)
-    return 'Test'
+    new_task = Task(id=task_id_control, title=data['title'], description=data.get("description", ""))
+    task_id_control +=1
+    tasks.append(new_task)
+    print(tasks)
+    return jsonify({"message": "Nova tarefa criada com sucesso"})
 
 
 if __name__ == "__main__":
