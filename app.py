a1ee6e3011aa1a42cee1889a9b06b1d268db278a
Stephano <stegoodds@gmail.com>
2024-02-26 16:48:55 -0300

    Insert search task by ID

--- app.py
+++ app.py
@@ -23,9 +23,17 @@ def get_tasks():
 
     output = {
                 "tasks": task_list,
-                "total_tasks": 0
+                "total_tasks": len(task_list)
                 }
     return jsonify(output)
+
+@app.route('/tasks/<int:id>', methods=['GET'])
+def get_task(id):
+    for t in tasks:
+        if t.id == id:
+            return jsonify(t.to_dict())
+    
+    return jsonify({"message": "Não foi possível encontrar a atividade"}), 404
 
 
 if __name__ == "__main__":
