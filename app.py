d7eb0c5160364999ad20a5aa6e7841b216365217
Stephano <stegoodds@gmail.com>
2024-02-26 17:57:32 -0300

    Include DELETE

--- app.py
+++ app.py
@@ -36,5 +36,39 @@ def get_task(id):
     return jsonify({"message": "Não foi possível encontrar a atividade"}), 404
 
 
+@app.route('/tasks/<int:id>', methods=['PUT'])
+def update_task(id):
+    
+    
+    task = None
+    for t in tasks:
+        if t.id == id: 
+            task = t
+    print(task)
+    if task == None:
+        return jsonify({"message": "Não foi possível encontrar a atividade"}), 404    
+
+    data = request.get_json()
+    task.title =  data['title']
+    task.description =  data['description']
+    task.completed =  data['completed']
+    print(task)
+    return jsonify({"message": "Tarefa atualizada com sucesso"})
+
+
+@app.route('/tasks/<int:id>', methods=['DELETE'])
+def delete_task(id):
+    task = None
+    for t in tasks:
+        if t.id == id:
+            task = t
+            break
+
+    if not task:
+        return jsonify({"message": "Não foi possível encontrar a atividade"}), 404
+
+    tasks.remove(task)
+    return jsonify({"message": "Tarefa deletada com sucesso"})
+
 if __name__ == "__main__":
     app.run(debug=True)
