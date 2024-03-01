4449839fc30bb99a204a20c8ebaed799f711a9a5
Stephano <stegoodds@gmail.com>
2024-03-01 11:49:59 -0300

    Updating tests & App

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def create_task():
     task_id_control +=1
     tasks.append(new_task)
     print(tasks)
-    return jsonify({"message": "Nova tarefa criada com sucesso"})
+    return jsonify({"message": "Nova tarefa criada com sucesso", "id": new_task.id})
 
 @app.route('/tasks', methods=['GET'])
 def get_tasks():
